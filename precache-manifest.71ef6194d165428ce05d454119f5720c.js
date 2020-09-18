8494b49c972d4184754ba581b6f0c53cf3b1a903
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-18 10:02:35 -0500

    Updates

---
+++ precache-manifest.71ef6194d165428ce05d454119f5720c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223c72aa0b8d5257470b3a3c857f76f5",
+    "url": "/Task/index.html"
+  },
+  {
+    "revision": "5b0034405d991e1d0bb2",
+    "url": "/Task/static/css/main.0eb395b8.chunk.css"
+  },
+  {
+    "revision": "fa40291ce538b3eb9526",
+    "url": "/Task/static/js/2.7bbfc8ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Task/static/js/2.7bbfc8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0034405d991e1d0bb2",
+    "url": "/Task/static/js/main.50ad3092.chunk.js"
+  },
+  {
+    "revision": "30f7852f7ee591081110",
+    "url": "/Task/static/js/runtime-main.11c71ff3.js"
+  }
+]);
